49dcf4e28d6390003131fc6d62c3fe70ec29cfcb
Sophia Aramburu <sophiaconstance12@gmail.com>
2022-11-07 20:21:38 -0800

    tested tailwind

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes);
+// router.use('/api', apiRoutes);
+router.get("/", (req, res)=> {
+    res.render("layouts/main")
+}) 
 
 module.exports = router;
